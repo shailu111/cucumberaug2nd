49401c83b7a85f4b25780f159e25a498f68055ba
lohan <lohan@DESKTOP-7D6O6U8.fios-router.home>
2021-04-29 16:21:28 -0400

    All Qa training document by shailaja 4/28/21

---
+++ src/main/java/CucumberAug2nd/CucumberAug2nd/App.java
@@ -0,0 +1,13 @@
+package CucumberAug2nd.CucumberAug2nd;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
